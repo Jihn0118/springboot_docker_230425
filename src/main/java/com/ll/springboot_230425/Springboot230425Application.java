8292fbe8478ed62864dc814a864bf233d771a529
Jihn0118 <lantern50@tukorea.ac.kr>
2023-04-25 09:16:11 +0900

    세팅

---
+++ src/main/java/com/ll/springboot_230425/Springboot230425Application.java
@@ -0,0 +1,13 @@
+package com.ll.springboot_230425;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot230425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot230425Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/springboot_230425/Springboot230425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springboot_230425;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot230425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
